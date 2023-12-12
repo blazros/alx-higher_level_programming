9b1e87dff539c575256accffe1260ae1c1f33104
ahmedos1999 <ah_os5@yahoo.com>
2023-12-12 23:36:40 +0200

    6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+module.exports = class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+};
